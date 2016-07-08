450d28cd7e57bb9c721f3f6b4eeadce71ed70bf8
vishal <vishal@cookedspecially.com>
2016-07-08 13:39:25 +0530

    Test WCF Program

---
+++ ClientConsoleApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service1Client client = new Service1Client();
+            Console.WriteLine(client.GetData(428));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApplicationUsingServiceRef/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationUsingServiceRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+            Console.WriteLine(client.GetData(420));
+            Console.ReadLine();
+        }
+    }
+}
